adfb9503d163cebcdcae5db1a5bcb257bc40476e
Kadin Tang <75419216+kadintang1@users.noreply.github.com>
2023-02-28 21:13:38 -0800

    Create 1610E.cpp

---
+++ 1610E.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define nl "\n"
+#define vi vector<int>
+#define trav(a, x) for(auto &a : x)
+#define pb push_back
+#define sz(v) (int)((v).size())
+#define all(v) v.begin(), v.end()
+#define bitCount(i) __builtin_popcount(i)
+#define FOR(i, a, b) for(int i = a; i < b; ++i)
+#define ROF(i, b, a) for(int i = b; i >= a; --i)
+#define arr array
+
+template<class T> bool umin(T& a, T b){
+    return b<a?a=b,1:0;
+}
+
+template<class T> bool umax(T& a, T b){
+    return a<b?a=b,1:0;
+}
+
+template<class T> T inv(T a, T b){
+    assert(a != 0); return 1<a ? b - inv(b%a,a)*b/a : 1;
+}
+
+template<class R, class S> pair<R, S> operator+(const pair<R, S>& p1, const pair<R, S>& p2){
+    return pair<R, S>(p1.first+p2.first, p1.second+p2.second);
+}
+
+template<class R, class S> pair<R, S> operator-(const pair<R, S>& p1, const pair<R, S>& p2){
+    return pair<R, S>(p1.first-p2.first, p1.second-p2.second);
+}
+
+template<class R, class S> pair<R, S> operator*(const pair<R, S>& p1, const pair<R, S>& p2){
+    return pair<R, S>(p1.first*p2.first, p1.second*p2.second);
+}
+
+#define dbg(x) cerr << #x << ": " << x << nl
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '['; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << ']'; }
+
+void solve(){
+    int n, res = 0; cin >> n;
+    vector<int> a(n);
+    trav(x, a) cin >> x;
+    FOR(i,0,n){
+        if(i-1>=0&&a[i]==a[i-1]) continue;
+        int cur_ans = 0, pos = i;
+        while(pos < n){
+            int idx = lower_bound(a.begin()+pos+1, a.end(), 2*a[pos]-a[i]) - a.begin();
+            ++cur_ans; pos = idx;
+        }
+        umax(res, cur_ans);
+    }
+    cout << n-res << nl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; cin >> t;
+    FOR(i,0,t){
+        solve();
+    }
+
+    return 0;
+}
